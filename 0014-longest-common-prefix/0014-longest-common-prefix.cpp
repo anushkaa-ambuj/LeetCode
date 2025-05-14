ba7d42d6b4867bdeb1a19fb02aa48efaa6214e9b
Anushkaa Ambuj <92167974+anushkaa-ambuj@users.noreply.github.com>
2025-05-14 12:42:17 +0530

    Time: 0 ms (100%), Space: 12.1 MB (34.77%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -4,6 +4,10 @@ public:
         int total_str = strs.size();
         string prefix = strs[0];
 
+        if (strs.size() == 0){
+            return "";
+        }
+
         for (int i=1;i<strs.size();i++){
             while (strs[i].find(prefix) != 0){
                 prefix = prefix.substr(0, prefix.length()-1);
