4132727dc907dacb49e8456094e84bffcec3b5cf
Anushkaa Ambuj <92167974+anushkaa-ambuj@users.noreply.github.com>
2025-06-12 12:10:56 +0530

    Time: 0 ms (100%), Space: 122 MB (46.43%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -14,17 +14,19 @@ public:
         if (head==NULL || head->next==NULL)
             return true;
         
+        // Step1: Find middle of linkedlist
         ListNode *slow = head;
         ListNode *fast = head;
-        
         while (fast->next != NULL && fast->next->next != NULL){
             slow = slow->next;
             fast = fast->next->next;
         }
         
+        // Step2: Reverse the second half
         slow->next = reverseList(slow->next);
         slow = slow->next;
         
+        // Step3: Comparing first half and second half.
         while (slow != NULL){
             if (head->val != slow->val)
                 return false;
