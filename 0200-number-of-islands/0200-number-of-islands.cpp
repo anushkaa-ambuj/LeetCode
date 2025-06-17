737c80891dfb4823142ebe9bbbe19af0d53cf909
Anushkaa Ambuj <92167974+anushkaa-ambuj@users.noreply.github.com>
2025-06-17 21:38:25 +0530

    Time: 24 ms (79.24%), Space: 16.2 MB (77.72%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -6,13 +6,11 @@ public:
 
         int count = 0;
 
-        vector<vector<int>> visited(n, vector<int>(m, 0));
-
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(!visited[i][j] && grid[i][j]=='1'){
+                if(grid[i][j] == '1'){
                     count++;
-                    dfs(i, j, visited, grid);
+                    dfs(i, j, grid);
                 }
             }
         }
@@ -20,19 +18,19 @@ public:
     }
 
 private:
-    void dfs(int i, int j, vector<vector<int>>& visited, vector<vector<char>>& grid){
+    void dfs(int i, int j, vector<vector<char>>& grid){
         int n = grid.size();
         int m = grid[0].size();
 
-        if(i<0 || i>=n || j<0 || j>=m || visited[i][j] || grid[i][j]=='0'){
+        if(i<0 || i>=n || j<0 || j>=m || grid[i][j]!='1'){
             return;
         }
 
-        visited[i][j] = 1;
+        grid[i][j] = '2';
 
-        dfs(i, j+1, visited, grid);
-        dfs(i+1, j, visited, grid); 
-        dfs(i, j-1, visited, grid);
-        dfs(i-1, j, visited, grid);
+        dfs(i-1, j, grid); //up
+        dfs(i+1, j, grid); //down
+        dfs(i, j+1, grid); //right
+        dfs(i, j-1, grid); //left
     }
 };
