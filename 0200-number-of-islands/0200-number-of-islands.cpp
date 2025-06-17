3b0d682500f0a1215d636107d7a51515bafaa7d3
Anushkaa Ambuj <92167974+anushkaa-ambuj@users.noreply.github.com>
2025-06-17 21:26:48 +0530

    Time: 32 ms (29.2%), Space: 17.2 MB (54.66%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -3,14 +3,16 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> visited(n, vector<int>(m,0));
+
         int count = 0;
 
-        for (int i=0; i<n;i++){
-            for (int j=0;j<m;j++){
+        vector<vector<int>> visited(n, vector<int>(m, 0));
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
                 if(!visited[i][j] && grid[i][j]=='1'){
                     count++;
-                    bfs(i,j,visited,grid);
+                    dfs(i, j, visited, grid);
                 }
             }
         }
@@ -18,28 +20,19 @@ public:
     }
 
 private:
-    void bfs(int i, int j, vector<vector<int>> &visited, vector<vector<char>>& grid){
-        visited[i][j] = 1;
+    void dfs(int i, int j, vector<vector<int>>& visited, vector<vector<char>>& grid){
         int n = grid.size();
         int m = grid[0].size();
-        queue<pair<int,int>> q;
-        q.push({i,j});
 
-        while(!q.empty()){
-            int i = q.front().first;
-            int j = q.front().second;
-            q.pop();
-
-            // traverse neighbours & mark them if its land
-            vector<pair<int,int>> directions = {{-1,0}, {1,0}, {0,-1}, {0,1}}; // up, down, left, right
-            for (auto [dx, dy] : directions) {
-                int nrow = i + dx;
-                int ncol = j + dy;
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='1' && !visited[nrow][ncol]){
-                    visited[nrow][ncol] = 1;
-                    q.push({nrow,ncol});
-                }
-            }
+        if(i<0 || i>=n || j<0 || j>=m || visited[i][j] || grid[i][j]=='0'){
+            return;
         }
+
+        visited[i][j] = 1;
+
+        dfs(i, j+1, visited, grid);
+        dfs(i+1, j, visited, grid); 
+        dfs(i, j-1, visited, grid);
+        dfs(i-1, j, visited, grid);
     }
 };
