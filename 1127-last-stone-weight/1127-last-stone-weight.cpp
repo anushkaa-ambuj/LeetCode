9895c752f9b68734bf90a6a0035409ab0ce48605
Anushkaa Ambuj <92167974+anushkaa-ambuj@users.noreply.github.com>
2025-06-18 19:17:59 +0530

    Time: 0 ms (100%), Space: 10.1 MB (9.45%) - LeetHub

--- 1127-last-stone-weight/1127-last-stone-weight.cpp
+++ 1127-last-stone-weight/1127-last-stone-weight.cpp
@@ -3,29 +3,24 @@ public:
     int lastStoneWeight(vector<int>& stones) {
         priority_queue<int> pq; //max heap
 
+        // Add in heap
         for(int num: stones){
             pq.push(num); //Time: O(nlogn)
         }
 
+
         while(pq.size()>1){
             int y = pq.top();
             pq.pop();
+            
             int x = pq.top();
             pq.pop();
 
-            if(x == y){
-                continue;
-            }
-            else if(y>x){
+            if(x!=y){
                 pq.push(y-x);
             }
         }
 
-        if(pq.empty()){
-            return 0;
-        }
-        else{
-            return pq.top();
-        }
+        return pq.empty() ? 0 : pq.top();
     }
 };
