22334fa6d839951525fb2568b5b6edd72f871159
Anushkaa Ambuj <92167974+anushkaa-ambuj@users.noreply.github.com>
2026-01-08 21:37:09 +0530

    Time: 52 ms (39.15%), Space: 29 MB (70.45%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,7 +5,7 @@ public:
         vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
 
-        for (int i=0; i<n; i++){
+        for (int i=0; i<n-1; i++){
             if (i > 0 && nums[i] == nums[i - 1]) continue;
             int j=i+1, k=n-1;
             while (j<k){
